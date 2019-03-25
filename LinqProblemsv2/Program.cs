b7ff91129686b11c9c5338ceeac7a4768729b60a
Natalie Zavada <3littlebirds414@gmail.com>
2019-03-25 12:03:19 -0500

    Initial commit.  One problem complete.

---
+++ LinqProblemsv2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Problems
+{
+    //public class WordList
+    //{
+    //    List<string> words = new List<string>() { "the", "bike", "it", "tenth", "mathematics" };
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List<string> words = new List<string>() { "the", "bike", "it", "tenth", "mathematics" };
+            var words = new List<string>() { "the", "bike", "it", "tenth", "mathematics" };
+
+            var resultList = words.FindAll(delegate (string s) { return s.Contains("th"); });
+
+            foreach (var word in resultList)
+            {
+                //resultList.ForEach(s => Console.Writeline(s);
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+
+}
